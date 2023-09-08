f18dc98847635528f30062409784b1e317e0ef0b
atoivonen04 <144342917+atoivonen04@users.noreply.github.com>
2023-09-08 10:08:44 +0300

    new file

--- Healt1.cs
+++ Healt1.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Healt1 : MonoBehaviour
+public class HealthSystem : MonoBehaviour, Damageable
 {
-    // Start is called before the first frame update
+    public int maxHealth = 100;
+    private int currentHealth;
+
+    // Initialize current health to the max health when the object is created.
     void Start()
     {
-        
+        currentHealth = maxHealth;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TakeDamage(int damageAmount)
     {
-        
+        // Reduce the current health by the damage amount.
+        currentHealth -= damageAmount;
+
+        // Check if the object has been destroyed.
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        // Perform any death-related actions here.
+        // For example, destroy the object or play an animation.
+        Destroy(gameObject);
     }
 }
